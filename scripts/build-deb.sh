3c80e4646e13e1ef4fe9c0a400f1d5b165a75fb6
Brian Moran <brian.moran@sabantoag.com>
2024-05-23 17:21:46 -0500

    Add missing AT commands
    I fixed the postinst script by adding in some missing AT commands.

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#  No way to specify a source directory or build directory. So change dir
-cd ${SCRIPT_DIR}/../
-#  -uc: Do not sign changes list, -us: do not sign source file
-#  See man dpkg-buildpackage for more info
+# Get the script's current directory
+SCRIPT_DIR="$(dirname -- $(readlink -f "${0}"))"
+# Get the script's parent directory
+PARENT_DIR="$(dirname -- "${SCRIPT_DIR}")"
+
+# No way to specify a source directory or build directory. So change dir
+cd "${PARENT_DIR}"
+# -uc: Do not sign changes list, -us: do not sign source file
+# See man dpkg-buildpackage for more info
 debuild --host-type aarch64-linux-gnu -aarm64 -uc -us -B
-cd ${SCRIPT_DIR}
+cd "${SCRIPT_DIR}"
