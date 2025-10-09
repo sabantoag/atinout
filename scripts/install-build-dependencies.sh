6a8adf78b0063e37755385035404c7fed95902fd
gaynoraj19 <27155636+gaynoraj19@users.noreply.github.com>
2025-10-09 17:27:05 -0500

    Add github actions workflow

--- scripts/install-build-dependencies.sh
+++ scripts/install-build-dependencies.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(dirname -- $(readlink -f "${0}"))"
 wget -P "${SCRIPT_DIR}" http://ftp.de.debian.org/debian/pool/main/r/ruby-ronn/ronn_0.9.1-3_all.deb
 wget -P "${SCRIPT_DIR}" http://ftp.de.debian.org/debian/pool/main/r/ruby-ronn/ruby-ronn_0.9.1-3_all.deb
 
-sudo apt-get install -y \
+apt-get install -y \
 devscripts \
 gcc-aarch64-linux-gnu \
 dpkg-cross \
